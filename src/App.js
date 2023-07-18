55643ff1250188bba8ffb86313ea5816859bdb5a
sergeev176 <sergey_sergeev_176@mail.ru>
2023-07-18 12:04:52 +0300

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,25 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { addCashAction, getCashAction } from './store/cashReducer';
-import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
+import { increment, decrement, addTodo, removeLastTodo } from './toolkitRedux/toolkitSlice';
 
 function App() {
 
+  const count = useSelector(state => state.toolkit.count);
+  const todos = useSelector(state => state.toolkit.todos);
   const dispatch = useDispatch();
-  const cash = useSelector(state => state.cash.cash);
-  const customers = useSelector(state => state.customers.customers);
-
-  const addCash = (cash) => {
-    dispatch(addCashAction(cash));
-  }
-
-  const getCash = (cash) => {
-    dispatch(getCashAction(cash));
-  }
-
-  const addCustomer = (name) => {
-    const customer = {
-      id: Date.now(),
-      name: name,
-    }
-    dispatch(addCustomerAction(customer));
-  }
-
-  const removeCustomer = (id) => {
-    dispatch(removeCustomerAction(id));
-  }
 
   return (
     <div>
-      <h1>hello</h1>
-      <div>{cash}</div>
-      <button onClick={() => addCash(Number(prompt()))}>add</button>
-      <button onClick={() => getCash(Number(prompt()))}>get</button>
-      <button onClick={() => addCustomer(prompt())}>добавить клиентов</button>
-
-      {customers.length > 0 ?
-        <div>
-          {customers.map(item => <div onClick={() => removeCustomer(item.id)}>{item.name}</div>)}
-        </div>
-        :
-        <div>
-          клиентов пока нет
-        </div>
-      }
+      <div>{count}</div>
+      <br />
+      <button onClick={() => dispatch(increment())}>add</button> 
+      <br />
+      <button onClick={() => dispatch(decrement())}>get</button>
+      <br />
+      <button onClick={() => dispatch(addTodo(prompt()))}>add todo</button>
+      <br />
+      <button onClick={() => dispatch(removeLastTodo())}>remove last</button>
+      {todos.map(todo => <div>{todo}</div>)}
     </div>
   );
 }
